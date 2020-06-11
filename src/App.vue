e3020567a815c607df8eeb88ce1a9a405dde440d
Nathan Chan <NChan@nbbj.com>
2020-06-11 13:48:50 -0700

    initial commit with test data download available

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <h2>This is an app that tests the json to csv converter</h2>
+    <h4>The app uses the two npm modules: jsonexport and file-saver</h4>
+    <br />
+    <button v-on:click="exportData">
+      Download Test Data
+    </button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import consultingData from "./data/ConsultingStudioFromHome.json";
+const jsonexport = require("jsonexport");
+import { saveAs } from "file-saver";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  methods: {
+    exportData() {
+      jsonexport(consultingData, (err, csv) => {
+        if (err) {
+          return console.log(err);
+        }
+        let blob = new Blob([csv]);
+        saveAs(blob, "ConsultingStudioFromHome.csv");
+      });
+    },
+  },
+};
 </script>
 
 <style>
